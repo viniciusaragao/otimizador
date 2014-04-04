5de7d28ee40c241142d2ffdb26d2dc588e4a62af
Yuri Araújo <yschmitzz@hotmail.com>
2014-04-04 10:00:35 -0300

    remoção do MQ, finalização da api

--- otimizador.cmd
+++ otimizador.cmd
@@ -1,6 +1,10 @@
+@echo off
 taskkill /F /im ExecutadorDeProcessosEmFila.exe
 taskkill /F /im ExecutadorDeObservadoresEmFila.exe
 taskkill /F /im MAAB.RestApi.exe
+taskkill /F /im MAAB.RestApi.vshost.exe
+taskkill /F /im MAAB.MQ.exe
+
  
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "%~dp0\MAAB.RestApi\MAAB.RestApi.sln" /t:Rebuild /p:Configuration=Debug
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "%~dp0\MAAB.TestesAceitacao\CargaDeDadosAPI\CargaDeDadosAPI.csproj" /t:Rebuild /p:Configuration=Debug
@@ -12,8 +16,6 @@ mkdir C:\MaabDBs\Global
 copy "%~dp0\MAAB.RestApi\MAAB.RestApi\ArquivosDefault\global.db" "C:\MaabDBs\Global\global.db"
  
 D:
-cd "%~dp0\MAAB.RestApi\MAAB.MQ\bin\Debug"
-start "MAAB.MQ" "%~dp0\MAAB.RestApi\MAAB.MQ\bin\Debug\MAAB.MQ.exe"
  
 cd "%~dp0\MAAB.RestApi\MAAB.RestApi\bin\Debug" 
 
@@ -25,5 +27,6 @@ start "Carga de dados" "%~dp0\MAAB.TestesAceitacao\CargaDeDadosAPI\bin\Debug\Car
 taskkill /F /im ExecutadorDeProcessosEmFila.exe
 taskkill /F /im ExecutadorDeObservadoresEmFila.exe
 taskkill /F /im MAAB.RestApi.exe
+
 cd "%~dp0\MAAB.RestApi\MAAB.RestApi\bin\Debug"
 start "MAAB.RestApi" "%~dp0\MAAB.RestApi\MAAB.RestApi\bin\Debug\MAAB.RestApi.exe"
